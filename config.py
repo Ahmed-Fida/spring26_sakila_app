42b2fac11420e64713b86eebb5bb1ea6bde4316a
Ahmed Fida <ahmedfida583@gmail.com>
2026-04-28 22:19:32 +0500

    fix: add Config class to config.py for app import

--- config.py
+++ config.py
@@ -8,10 +8,12 @@
 
 import os
 
-MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
-MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
-SECRET_KEY = os.environ.get('SECRET_KEY')
-CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
-HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+class Config:
+    """Base configuration class for the Sakila Flask application."""
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+    HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
